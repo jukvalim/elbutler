1423e62e9db2c06b0e8c600c19327ad9a64c5ea0
jukvalim <jukka.valimaa@gmail.com>
2020-08-14 11:26:20 +0300

    Update PoD chapter regex

--- lib/elbutler/tasks.ex
+++ lib/elbutler/tasks.ex
@@ -2,7 +2,7 @@ defmodule ElButler.Tasks do
   @wtc_chapter_regex ~r|<dd class="chapters">.*>(?<chapter>[[:digit:]]+)</a>[/\w]|
   @hoc_chapter_regex ~r|/fiction/32502/heart-of-cultivation/chapter/(?<chapter_id>[[:digit:]]+)/(?<chapter>[[:digit:]]+)-|
   @dcc_chapter_regex ~r|/fiction/29358/dungeon-crawler-carl/chapter/(?<chapter_id>[[:digit:]]+)/chapter-(?<chapter>[[:digit:]]+)|
-  @pod_chapter_regex ~r|<a style="" class='chapter-item' href="/Paragon-of-Destruction/(?<chapter_id>[[:digit:]]+).html"><div class='chapter-info'><p class='chapter-name'>(?<chapter>[[:digit:]]+) [\w\s]+</p></div></a>|
+  @pod_chapter_regex ~r|<a class='chapter-item' href="/Paragon-of-Destruction/(?<chapter_id>[[:digit:]]+).html"><div class='chapter-info'><p class='chapter-name'>(?<chapter>[[:digit:]]+) [\w\s]+</p></div></a>|
 
   def check_worth_the_candle() do
     check_webfiction(
