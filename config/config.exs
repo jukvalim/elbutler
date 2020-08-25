c6e34571389beff4faad3932b6297b2ca90aa30a
jukvalim <jukka.valimaa@gmail.com>
2020-08-25 09:42:20 +0300

    Add FoD to hourly run tasks

--- config/config.exs
+++ config/config.exs
@@ -6,13 +6,11 @@ config :logger,
 config :elbutler, ElButler.Scheduler,
   timezone: "Europe/Helsinki",
   jobs: [
-    # {"@hourly", fn -> ElButler.Tasks.check_worth_the_candle() end},
     {"@hourly", {ElButler.Tasks, :check_worth_the_candle, []}},
-    # {"@hourly", fn -> ElButler.Tasks.check_hoc() end},
     {"@hourly", {ElButler.Tasks, :check_hoc, []}},
     {"@hourly", {ElButler.Tasks, :check_dcc, []}},
-    # {"@hourly", fn -> ElButler.Tasks.check_pod() end}
     {"@hourly", {ElButler.Tasks, :check_pod, []}}
+    {"@hourly", {ElButler.Tasks, :check_fod, []}}
     # {"* * * * *",      fn ->  System.cmd("say", [("minute " <> Integer.to_string DateTime.utc_now().minute)]) end},
     # {{:extended, "* * * * *"}, fn -> System.cmd("say", [Integer.to_string DateTime.utc_now().second]) end},
     # Every minute
